0a564476238244305d17493d447ac8d1218e16d7
Orbie <vozach1234@gmail.com>
2022-02-17 12:09:20 -0500

    Project4

---
+++ JavaScript Projects/Project4_dictionaries/scripts/main.js
@@ -0,0 +1,11 @@
+function my_Dictionary() {
+    var Animal = {
+        species: "Dog",
+        color: "Black",
+        breed: "Labrador",
+        age: 5,
+        sound: "Bark!"
+    };
+    delete Animal.sound;
+    document.getElementById("dictionary").innerHTML = Animal.species;
+}
