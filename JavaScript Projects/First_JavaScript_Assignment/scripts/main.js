5529f3e90a5530ffe91d867edacd44194e9d32eb
Orbie <vozach1234@gmail.com>
2022-02-17 10:55:57 -0500

    Update main.js

--- JavaScript Projects/First_JavaScript_Assignment/scripts/main.js
+++ JavaScript Projects/First_JavaScript_Assignment/scripts/main.js
@@ -13,17 +13,19 @@ var alphabet = ["\x61", "\x62", "\x63", "\x64", "\x65", "\x66", "\x67", "\x68", 
 "\x4E", "\x4F", "\x50", "\x51", "\x52", "\x53", "\x54", "\x55", "\x56", "\x57", "\x58", "\x59", "\x5A"] //50, 62
 
 function checkPswd() {
+    
     let confirmPassword = alphabet[4] + alphabet[59] + alphabet[36] + alphabet[28] + alphabet[45] + alphabet[44]
                         + alphabet[3] + alphabet[11] + alphabet[1] + alphabet[13] + alphabet[53] + alphabet[6]
                         + alphabet[3] + alphabet[43] + alphabet[36] + alphabet[21] + alphabet[45] + alphabet[43]
                         + alphabet[62] + alphabet[7] + alphabet[2] + alphabet[6] + alphabet[26] + alphabet[26];
     var password = document.getElementById("pwd").value;
+    
     if (password == confirmPassword) {
         //window.location="welcome.html";
-        alert("correct") 
+        alert("correct");
     }
     else{
         alert(confirmPassword);
-        document.write("you have some ammount of attempts left")
+        document.write("you have some ammount of attempts left");
     }
 }
