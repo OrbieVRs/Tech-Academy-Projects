bf57a00bc6567d0772579e6041a890fc59761b11
Orbie <vozach1234@gmail.com>
2022-02-17 11:08:28 -0500

    Adding Operators

---
+++ JavaScript Projects/Adding_Operators/scripts/main.js
@@ -0,0 +1,5 @@
+function myFunction() { // calling a function
+    var sentence = "I am learning";
+    sentence += " a lot from this book!"; // adding this string to the variable sentence
+    document.getElementById("Concatenate").innerHTML = sentence; // action
+}
