5e36a7b95aa38db9ae73ac9d22b6e2c91fa376af
Orbie <vozach1234@gmail.com>
2022-02-18 13:21:25 -0500

    Update

--- JavaScript Projects/Project7_scope_time_function/scripts/main.js
+++ JavaScript Projects/Project7_scope_time_function/scripts/main.js
@@ -1,6 +1,6 @@
-var X = 10;
+var X = 10; // global variable
 function Add_numbers_1() {
-    var Y = 10
+    var Y = 10 // local variable
     document.write(20 + X + "<br>");
     console.log(15 + Y);
 }
@@ -8,42 +8,43 @@ function Add_numbers_2() {
     document.write(X + 100);
     console.log(Y + 100);
 }
+//calling the functions
 Add_numbers_1();
 Add_numbers_2();
 
-if (1 < 2) {
+if (1 < 2) { // Always true and if statement runs
     document.write("The left number is smaller than the number on the right.");
 }
 
 function get_Date() {
-    if (new Date().getHours() < 18) {
+    if (new Date().getHours() < 18) { // If the time is before 6:00pm true, else program doesnt run
         document.getElementById("Greeting").innerHTML = "How are you today?";
     }
 }
 
 // Age
 function Age_Function() {
-    Age = document.getElementById("Age").value;
+    Age = document.getElementById("Age").value; // gets value of input, saves to age
     if (Age >= 18) {
         Vote = "You are old enough to vote!";
     }
     else {
         Vote = "You are not old enough to vote!";
     }
-    document.getElementById("How_old_are_you?").innerHTML = Vote;
+    document.getElementById("How_old_are_you?").innerHTML = Vote; //
 }
 
 //Time
 function Time_Function() {
     var Time = new Date().getHours();
     var Reply;
-    if (Time < 12 == Time > 0) {
+    if (Time < 12 == Time > 0) { // True if time is after midnight (12:00am) and before midday (12:00pm)
         Reply = "It is morning time!";
     }
-    else if (Time >= 12 == Time < 18) {
+    else if (Time >= 12 == Time < 18) { // True if time is during or after midday (12:00pm) and before 6:00pm
         Reply = "It is afternoon.";
     }
-    else {
+    else { // Unconditional true if the previous are false
         Reply = "It is evening time.";
     }
     document.getElementById("Time_of_day").innerHTML = Reply;
--- JavaScript Projects/Project8_string_methods/scripts/main.js
+++ JavaScript Projects/Project8_string_methods/scripts/main.js
@@ -4,25 +4,25 @@ function full_Sentence() {
     var part_2 = "made this ";
     var part_3 = "into a complete ";
     var part_4 = "sentence.";
-    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    var whole_sentence = part_1.concat(part_2, part_3, part_4); //combines part_1 with part_2 thru 4 into whole_sentence
     document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
 
 function slice_Method() {
     var Sentence = "All work and no play makes Johnny a dull boy.";
-    var Section = Sentence.slice(27,33);
+    var Section = Sentence.slice(27,33); //takes the 27th through 33rd characters from sentence, assigns them to section
     document.getElementById("Slice").innerHTML = Section;
 }
 
 // int to String
 function string_Method() {
     var X = 182;
-    document.getElementById("Numbers_to_string").innerHTML = X.toString();
+    document.getElementById("Numbers_to_string").innerHTML = X.toString(); // converts X to a string
 }
 
 // precision
 function precision_Method() {
     var X = 12938.3012987376112;
-    document.getElementById("Precision").innerHTML = X.toPrecision(10);
+    document.getElementById("Precision").innerHTML = X.toPrecision(10); // takes the first 10 characters
 }
 
